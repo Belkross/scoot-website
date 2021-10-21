04324e1086616f879db3e3e8e24d3fb31635f063
Belkross <belkross.pro@gmail.com>
2021-10-21 22:46:36 +0200

    page index: add content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import data from "../content/PageIndex.yaml"
 
 const PageIndex = () => {
+  console.log(data)
   return <Layout></Layout>;
 };
 
