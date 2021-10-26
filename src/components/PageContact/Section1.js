e64a452cb4b5695107d05f158b98b42e59db64a8
Belkross <belkross.pro@gmail.com>
2021-10-26 20:12:06 +0200

    test netlify form

---
+++ src/components/PageContact/Section1.js
@@ -0,0 +1,33 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
+function Section1() {
+  return (
+    <Stack component="section" sx={sx_container}>
+      <form name="contactBelkross" netlify style={style_form}>
+        <TextField label="Nom" name="nom"/>
+        <TextField label="Message" name="message" multiline sx={sx_message}/>
+        <Button type="submit">Valider</Button>
+      </form>
+    </Stack>
+  );
+}
+
+export default Section1;
+
+const sx_container = {
+  alignItems: "center",
+  p: 4,
+};
+
+const style_form = {
+  display: "flex",
+  alignItems: "center",
+  gap: 12
+}
+
+const sx_message = {
+  minWidth: "400px"
+}
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+import Section1 from "../components/PageContact/Section1.js";
+/* import Container from "@mui/material/Container"; */
+
+const PageContact = () => {
+  return (
+    <Layout>
+      <Section1 />
+    </Layout>
+  );
+};
+
+export default PageContact;
