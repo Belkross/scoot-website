3dc013e7914af5921e3e4c2b984623a3b9b5f233
Belkross <belkross.pro@gmail.com>
2021-10-29 20:42:44 +0200

    fix faq page2

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -2,8 +2,8 @@ import React from "react";
 import Layout from "../components/Layout/Layout";
 import Container from "@mui/material/Container";
 import PageHeader from "../components/PageHeader";
-import Section1 from "../components/PageFaq/Section1.js";
-import Section2 from "../components/PageFaq/Section2.js";
+import Section1 from "../components/PageQuestions/Section1.js";
+import Section2 from "../components/PageQuestions/Section2.js";
 
 import { graphql } from "gatsby";
 
