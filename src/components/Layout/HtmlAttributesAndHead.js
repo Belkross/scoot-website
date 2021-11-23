7c7710c26adc8e04d86157512461f1c1b0f2c4cd
Belkross <belkross.pro@gmail.com>
2021-11-23 06:19:33 +0100

    feature: link favicon to cms, deliver different sizes

--- src/components/Layout/HtmlAttributesAndHead.js
+++ src/components/Layout/HtmlAttributesAndHead.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 import { Helmet } from "react-helmet";
 import { PageContext } from "../MyComponents/PageContextProvider";
 const CHARSET = "utf-8";
 
 export default function HtmlAttributesAndHead() {
+  const data = useStaticQuery(query);
   const context = React.useContext(PageContext);
+  
+  const array_favicon = data.datoCmsFaviconMetaTags.tags.filter(
+    (element) => element.tagName === "link"
+  );
+  const list_favicon = array_favicon.map((markup) => {
+    const { rel, sizes, href, type } = markup.attributes;
+    return <link key={sizes} rel={rel} sizes={sizes} href={href} type={type} />;
+  });
 
   return (
     <Helmet
@@ -14,11 +24,19 @@ export default function HtmlAttributesAndHead() {
     >
       {/* <title>{siteMetadata.title}</title> */}
       <meta charset={CHARSET} />
-      {/* <link rel="icon" href={favicon} type="image/x-icon" /> */}
+      {list_favicon}
       {/* <meta name="description" content={siteMetadata.description} /> */}
     </Helmet>
   );
 }
+
+const query = graphql`
+  query component_HtmlAttributesAndHead {
+    datoCmsFaviconMetaTags {
+      tags
+    }
+  }
+`;
 
 /* 
 I also can to modify the <head/> from .html but not dynamically. That’s why I 
