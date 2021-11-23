115c94d65f7e9de8eb63f358585f3b2e0020da66
Belkross <belkross.pro@gmail.com>
2021-11-23 05:36:49 +0100

    refacto component HtmlAttributesAndHead, rewrite lang and charset

--- src/components/Layout/HtmlAttributesAndHead.js
+++ src/components/Layout/HtmlAttributesAndHead.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { PageContext } from "../MyComponents/PageContextProvider";
+const CHARSET = "utf-8";
 
-function HtmlAttributesAndHead({ siteMetadata, favicon, language }) {
+export default function HtmlAttributesAndHead() {
+  const context = React.useContext(PageContext);
+
   return (
     <Helmet
       htmlAttributes={{
-        lang: language,
+        lang: context.locale,
       }}
     >
-      <title>{siteMetadata.title}</title>
-      <meta charset={siteMetadata.charset} />
-      <link rel="icon" href={favicon} type="image/x-icon" />
-      <meta name="description" content={siteMetadata.description} />
+      {/* <title>{siteMetadata.title}</title> */}
+      <meta charset={CHARSET} />
+      {/* <link rel="icon" href={favicon} type="image/x-icon" /> */}
+      {/* <meta name="description" content={siteMetadata.description} /> */}
     </Helmet>
   );
 }
 
-export default HtmlAttributesAndHead;
+/* 
+I also can to modify the <head/> from .html but not dynamically. That’s why I 
+use react-helmet
+*/
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,23 +2,16 @@ import React from "react";
 import HtmlAttributesAndHead from "./HtmlAttributesAndHead";
 import Header from "./Header";
 import Footer from "./Footer";
-import { useStaticQuery, graphql } from "gatsby";
 import PageTransition from "./PageTransition";
 import ContainerVertical from "./ContainerVertical";
 import DialogFakeWebsite from "../MyComponents/DialogFakeWebsite";
 import PageContextProvider from "../MyComponents/PageContextProvider";
 
-function Layout({ children, pageContext }) {
-  const data = useStaticQuery(query);
-
+export default function Layout({ children, pageContext }) {
   return (
     <PageContextProvider pageContext={pageContext}>
       <DialogFakeWebsite>
-        <HtmlAttributesAndHead
-          siteMetadata={data.site.siteMetadata}
-          favicon={data.contentYaml.general.favicon.publicURL}
-          language="fr"
-        />
+        <HtmlAttributesAndHead />
         <ContainerVertical>
           <Header />
           <PageTransition>{children}</PageTransition>
@@ -28,43 +21,3 @@ function Layout({ children, pageContext }) {
     </PageContextProvider>
   );
 }
-
-export default Layout;
-
-const query = graphql`
-  query component_layout {
-    site {
-      siteMetadata {
-        description
-        title
-        charset
-      }
-    }
-
-    contentYaml {
-      general {
-        favicon {
-          publicURL
-        }
-        social_media {
-          facebook {
-            name
-            website
-          }
-          instagram {
-            name
-            website
-          }
-          linkedin {
-            name
-            website
-          }
-          twitter {
-            name
-            website
-          }
-        }
-      }
-    }
-  }
-`;
