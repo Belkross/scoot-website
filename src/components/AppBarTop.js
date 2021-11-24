b12d7c4348a6077b6f520d8cb96f26a7894ffede
Belkross <belkross.pro@gmail.com>
2021-11-24 13:38:12 +0100

    build(Layout): add a responsive margin bottom to Header

--- src/components/AppBarTop.js
+++ src/components/AppBarTop.js
@@ -2,7 +2,7 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 
-function AppBarTop({ children }) {
+export default function AppBarTop({ children }) {
   return (
     <AppBar position="sticky" sx={sx_container}>
       <Toolbar>{children}</Toolbar>
@@ -10,13 +10,10 @@ function AppBarTop({ children }) {
   );
 }
 
-export default AppBarTop;
-
 const sx_container = {
   backgroundColor: "background.navBar",
   backgroundImage: "none",
+  mb: { xs: 4, md: 6, lg: 8 },
 };
 
-/*
-AppBar is a header markup by default
-*/
+/*AppBar is a header markup by default*/
