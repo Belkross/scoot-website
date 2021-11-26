9b90d0012daddb4c18fbf56e22fb78437752620c
Belkross <belkross.pro@gmail.com>
2021-11-26 20:48:26 +0100

    style(component): improve text contrast

--- src/components/MyComponents/ButtonLocale.js
+++ src/components/MyComponents/ButtonLocale.js
@@ -5,40 +5,35 @@ import { PageContext } from "./PageContextProvider";
 import { navigate } from "gatsby";
 
 export default function ButtonLocale() {
-  const context = React.useContext(PageContext);
-  const { slug: currentSlug, supportedLocales, getPathname } = context;
+	const context = React.useContext(PageContext);
+	const { slug: currentSlug, supportedLocales, getPathname } = context;
 
-  const handleChange = (event, newLocale) => {
-    if (newLocale) navigate(getPathname(newLocale, currentSlug));
-    //if you click on the same Button, newLocale = null
-  };
+	const handleChange = (event, newLocale) => {
+		if (newLocale) navigate(getPathname(newLocale, currentSlug));
+		//if you click on the same Button, newLocale = null
+	};
 
-  const list_ToggleButton = supportedLocales.map((locale) => {
-    return (
-      <ToggleButton
-        key={locale}
-        value={locale}
-        children={locale}
-        sx={sx_ToggleButton}
-      />
-    );
-  });
+	const list_ToggleButton = supportedLocales.map((locale) => {
+		return (
+			<ToggleButton key={locale} value={locale} children={locale} sx={sx_ToggleButton} />
+		);
+	});
 
-  return (
-    <ToggleButtonGroup
-      color="secondary"
-      value={context.locale}
-      exclusive
-      onChange={handleChange}
-    >
-      {list_ToggleButton}
-    </ToggleButtonGroup>
-  );
+	return (
+		<ToggleButtonGroup value={context.locale} exclusive onChange={handleChange}>
+			{list_ToggleButton}
+		</ToggleButtonGroup>
+	);
 }
 
 const sx_ToggleButton = {
-  width: 45,
+	width: 45,
+	borderColor: "text.primary",
+	"&.Mui-selected": {
+		backgroundColor: "action.selected",
+	},
 };
+
 /*
 The button needs an array of supported locales to scale. The single truth source comes from
 gatsby-node.js with a pageContext
--- src/theme/components.js
+++ src/theme/components.js
@@ -1,54 +1,54 @@
 import { createTheme } from "@mui/material/styles";
 
 export default function addComponents(theme) {
-  return createTheme(theme, {
-    components: {
-      MuiIconButton: {
-        variants: [
-          {
-            props: { variant: "navigation" },
-            style: {
-              border: "1px solid",
-              borderRadius: theme.shape.borderRadius,
-            },
-          },
-        ],
-      },
-      MuiAppBar: {
-        styleOverrides: {
-          root: {
-            backgroundColor: theme.palette.background.navBar,
-          },
-        },
-      },
-      MuiButton: {
-        variants: [
-          {
-            props: { variant: "navBar" },
-            style: {
-              backgroundColor: theme.palette.background.navBar,
-            },
-          },
-          {
-            props: { variant: "importantAction" },
-            style: {
-              backgroundColor: theme.palette.secondary.main,
-              color: theme.palette.text.dark
-            },
-          },
-        ],
-        styleOverrides: {
-          root: {
-            padding: "12px 12px",
-            color: theme.palette.text.primary,
-            "&:hover": {
-              backgroundColor: theme.palette.action.hover,
-            },
-          },
-        },
-      },
-    },
-  });
+	return createTheme(theme, {
+		components: {
+			MuiIconButton: {
+				variants: [
+					{
+						props: { variant: "navigation" },
+						style: {
+							border: "1px solid",
+							borderRadius: theme.shape.borderRadius,
+						},
+					},
+				],
+			},
+			MuiAppBar: {
+				styleOverrides: {
+					root: {
+						backgroundColor: theme.palette.background.navBar,
+					},
+				},
+			},
+			MuiButton: {
+				variants: [
+					{
+						props: { variant: "navBar" },
+						style: {
+							backgroundColor: theme.palette.background.navBar,
+						},
+					},
+					{
+						props: { variant: "importantAction" },
+						style: {
+							backgroundColor: theme.palette.secondary.main,
+							color: theme.palette.text.dark,
+						},
+					},
+				],
+				styleOverrides: {
+					root: {
+						padding: "12px 12px",
+						color: theme.palette.text.primary,
+						"&:hover": {
+							backgroundColor: theme.palette.action.hover,
+						},
+					},
+				},
+			},
+		},
+	});
 }
 
 /* 
